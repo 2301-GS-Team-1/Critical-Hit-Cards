814a820ad7ccea16b4181d9bd65c0b007f4446a0
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-04-19 11:50:11 -0500

    initial single card component

---
+++ client/features/cards/Single.Card.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+const SingleCard = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const card = useSelector(selectSingleCard);
+  const { firstName, lastName, imageUrl, information, price, quantity } = card;
+
+  useEffect(() => {
+    dispatch(fetchSingleStudent(id));
+  }, [dispatch]);
+
+  return (
+    <div id="single-card">
+      <div id="single-card-info">
+        <h1>
+          {firstName} {lastName}
+        </h1>
+        <img src={imageUrl} />
+        <p>{information}</p>
+        <p>${price}</p>
+        <p>In Stock: {quantity}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
