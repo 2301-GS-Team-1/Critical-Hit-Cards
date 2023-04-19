11084a4d29f9922f043f983065b1eed1591f0e8e
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-04-19 11:50:24 -0500

    initial all cards component

---
+++ client/features/cards/AllCards.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const Cards = () => {
+  const dispatch = useDispatch();
+  const cards = useSelector(selectCards);
+
+  useEffect(() => {
+    dispatch(fetchCards());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {cards.map((card) => {
+        return (
+          <div id="card-container" key={card.id}>
+            <img src={card.imageUrl} />
+            <div id="card-info">
+              <h3>
+                {card.firstName} {card.lastName}
+              </h3>
+              {/* <p>{card.information}</p> */}
+              <p>Price: {card.price}</p>
+              {/* <p>In Stock: {card.quantity}</p> */}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cards;
