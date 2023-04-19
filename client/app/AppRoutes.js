3521d3188ba013cb1b660489dd322585f88a7c35
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-04-19 15:18:04 -0500

    added test route allCards component

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import AuthForm from '../features/auth/AuthForm';
-import Home from '../features/home/Home';
-import { me } from './store';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import AuthForm from "../features/auth/AuthForm";
+import Cards from "../features/cards/AllCards";
+import Home from "../features/home/Home";
+import { me } from "./store";
 
 /**
  * COMPONENT
@@ -30,6 +31,8 @@ const AppRoutes = () => {
             path="/*"
             element={<AuthForm name="login" displayName="Login" />}
           />
+          {/* Implemented the route below for testing purposes. Need to decide where to be placed. */}
+          <Route path="/products" element={<Cards />} />
           <Route
             path="/login"
             element={<AuthForm name="login" displayName="Login" />}
