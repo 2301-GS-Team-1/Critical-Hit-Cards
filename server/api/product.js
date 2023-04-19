f4a14871355794977b60e0bcd7d02e2b79779917
Ray Mangahas <r.mangahas94@gmail.com>
2023-04-19 11:53:31 -0700

    created routes for cart (10% functionality) and product (100% functionality)

---
+++ server/api/product.js
@@ -0,0 +1,67 @@
+const router = require("express").Router();
+const {
+  models: { Product, Cart },
+} = require("../db");
+module.exports = router;
+
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.findAll({
+      // explicitly select only the firstName, lastName, imageUrl, information, price, quantity fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      // attributes: [
+      //   "firstName",
+      //   "lastName",
+      //   "imageUrl",
+      //   "information",
+      //   "price",
+      //   "quantity",
+      // ],
+    });
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const singleProduct = await Product.findByPk(req.params.id);
+    res.json(singleProduct);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  console.log(req.body);
+  try {
+    let newProduct = await Product.create(req.body);
+    res.statusCode(201).send(newProduct);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.destroy();
+    res.send(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.update(req.body);
+    res.send(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
