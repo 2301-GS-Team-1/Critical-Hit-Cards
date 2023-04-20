422c1a42b4e475d2f674c68a8fc6252c5649c878
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-04-20 16:02:17 -0500

    adjusted database keys and added beforeCreate hook

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -2,28 +2,30 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 
 const Product = db.define("product", {
-  firstName: {
+  name: {
     type: Sequelize.STRING,
     unique: true,
     allowNull: false,
   },
-  lastName: {
-    type: Sequelize.STRING,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
+  images: {
+    type: Sequelize.JSON,
     defaultValue:
       "https://www.travelandleisure.com/thmb/E5szi7N2r1eN-8b3vkl5STvWz9o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/notre-dame-university-COLLEGECAMP0421-039ce0bfd40b4e429b825c6fb6ebfaa6.jpg",
   },
-  information: {
+  flavorText: {
     type: Sequelize.STRING,
   },
   price: {
     type: Sequelize.INTEGER,
+    defaultValue: Math.floor(Math.random() * 1000),
   },
   quantity: {
     type: Sequelize.INTEGER,
   },
+});
+
+Product.beforeCreate((instance, options) => {
+  instance.price = Math.floor(Math.random() * (100 - 5 + 1)) + 5;
 });
 
 module.exports = Product;
