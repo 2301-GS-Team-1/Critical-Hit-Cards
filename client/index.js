f2cf14e15ac2a2a95ef786b48915c039ffbfb288
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-04-20 10:04:09 -0500

    removed unneccasary comment

--- client/index.js
+++ client/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./app/store.js";
-import App from "./app/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./app/App";
+import store from "./app/store.js";
 
 const root = createRoot(document.getElementById("app"));
 
@@ -14,4 +14,3 @@ root.render(
     </Provider>
   </Router>
 );
-//test nicks change
