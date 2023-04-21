8f9f71d2921d809c22a5fda698e3f50f9a6be982
Aaron Sprayberry <aaronsprayberry@icloud.com>
2023-04-21 16:56:38 -0500

    added more fields from the database

--- client/features/cards/SingleCard.js
+++ client/features/cards/SingleCard.js
@@ -7,20 +7,75 @@ const SingleCard = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const card = useSelector(selectSingleCard);
-  const { name, images, flavorText, price, quantity } = card;
+  const {
+    name,
+    supertype,
+    subtype,
+    hp,
+    evolvesFrom,
+    evolvesTo,
+    attacks,
+    weaknesses,
+    retreatCost,
+    number,
+    artist,
+    rarity,
+    nationalPokedexNumbers,
+    legalities,
+    images,
+    flavorText,
+    price,
+    quantity,
+  } = card;
+
+  // const attackName = attacks.map((attack) => attack.name);
 
   useEffect(() => {
     dispatch(fetchSingleCard(id));
   }, [dispatch]);
 
   return (
-    <div id="single-card">
+    <div id="single-card" key={id}>
+      <img src={images?.large} />
       <div id="single-card-info">
-        {/* <h1>{name}</h1> */}
-        <img src={images?.large} />
-        <p>{flavorText}</p>
-        <p>${price}</p>
-        <p>In Stock: {quantity}</p>
+        <h1>{name}</h1>
+        <p>
+          {supertype} - {subtype}
+        </p>
+        <p>HP {hp}</p>
+        <p>
+          Evolves from <em>{evolvesFrom}</em>
+        </p>
+        <p>
+          Evolves to <em>{evolvesTo}</em>
+        </p>
+        <div>
+          ATTACKS{" "}
+          {attacks && attacks.length > 0
+            ? attacks.map((attack, index) => (
+                <div key={index}>
+                  <p>
+                    {attack.name} - {attack.cost.join(", ")}
+                  </p>
+                  <p>
+                    Energy Cost: {attack.convertedEnergyCost}, Damage:{" "}
+                    {attack.damage}
+                  </p>
+                  <p>{attack.text}</p>
+                </div>
+              ))
+            : "None"}
+        </div>
+        {/* <p>WEAKNESSES {weaknesses}</p> */}
+        {/* <p>RETREAT COST{retreatCost}</p> */}
+        {/* <p>NUMBER {number}</p> */}
+        {/* <p>ARTIST {artist}</p> */}
+        {/* <p>RARITY {rarity}</p> */}
+        {/* <p>Pokédex Entry {nationalPokedexNumbers}</p> */}
+        {/* <p>Legalities {legalities}</p> */}
+        <p>{/* <em>{flavorText}</em> */}</p>
+        {/* <p>${price}</p> */}
+        {/* <p>In Stock: {quantity}</p> */}
       </div>
     </div>
   );
